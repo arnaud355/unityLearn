dfcb80bc42ad0517856e7a345d4a84f751543e17
arnaud355 <arnaud355bafana@gmail.com>
2022-01-26 12:41:11 +0100

    add of challenge 4

---
+++ Challenge4/Assets/Challenge 4/Scripts/PlayerControllerX.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    private float speed = 500.0f;
+    private float speedBoost = 1200.0f;
+    private GameObject focalPoint;
+
+    public bool hasPowerup = false;
+    public GameObject powerupIndicator;
+    public int powerUpDuration = 5;
+
+    private float normalStrength = 10.0f; // how hard to hit enemy without powerup
+    private float powerupStrength = 25.0f; // how hard to hit enemy with powerup
+
+    /* Mettre le prefab en enfant du focal point dans Player, et glisser ce prefab dans la variable dans Player
+       et non direcement le prefab du dossier Prefabs */
+    public ParticleSystem smokeParticle;
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+    }
+
+    void Update()
+    {
+        // Add force to player in direction of the focal point (and camera)
+        float verticalInput = Input.GetAxis("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRb.AddForce(focalPoint.transform.forward * verticalInput * speedBoost * Time.deltaTime, ForceMode.Impulse);            
+            smokeParticle.Play();
+        }
+        else
+        {           
+            playerRb.AddForce(focalPoint.transform.forward * verticalInput * speed * Time.deltaTime);
+
+            // Set powerup indicator position to beneath player
+            powerupIndicator.transform.position = transform.position + new Vector3(0, -0.6f, 0);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            smokeParticle.Stop();
+        }
+    }
+
+    // If Player collides with powerup, activate powerup
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Powerup"))
+        {
+            Destroy(other.gameObject);
+            hasPowerup = true;
+            powerupIndicator.SetActive(true);
+            StartCoroutine(PowerupCooldown());
+        }
+    }
+
+    // Coroutine to count down powerup duration
+    IEnumerator PowerupCooldown()
+    {
+        yield return new WaitForSeconds(powerUpDuration);
+        hasPowerup = false;
+        powerupIndicator.SetActive(false);
+    }
+
+    // If Player collides with enemy
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Rigidbody enemyRigidbody = other.gameObject.GetComponent<Rigidbody>();
+            //not this sentence : Vector3 awayFromPlayer =  transform.position - other.gameObject.transform.position;
+            // Calculation is enemy position - player's position to give the proper Vector (Distance + direction) force
+            Vector3 awayFromPlayer = (other.gameObject.transform.position - transform.position);
+
+            if (hasPowerup) // if have powerup hit enemy with powerup force
+            {
+                enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+            }
+            else // if no powerup, hit enemy with normal strength 
+            {
+                enemyRigidbody.AddForce(awayFromPlayer * normalStrength, ForceMode.Impulse);
+            }
+        }
+    }
+}
---
+++ Challenge4/Assets/Challenge 4/Scripts/SpawnManagerX.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public GameObject powerupPrefab;
+
+    private float spawnRangeX = 10.0f;
+    private float spawnZMin = 15.0f; // set min spawn Z
+    private float spawnZMax = 25.0f; // set max spawn Z
+
+    private int enemyCount = 0;
+    private int waveCount = 1;
+
+
+    public GameObject player;
+
+    // Déclare une instance de la classe EnemyX
+    private EnemyX enemyScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // On instancie l'object de la classe EnemyX en demandant le script EnemyX de l'object Enemy
+        /* On peut ainsi récupérer des proppriétés et method qui s'appliqueront sur les objets
+         par exemple : enemyScript.speed */
+        enemyScript = enemyPrefab.GetComponent<EnemyX>();
+
+        SpawnEnemyWave(waveCount);
+        Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Renvoi le nombre d'objects (un int) ayant le tag Enemy
+        enemyCount = GameObject.FindGameObjectsWithTag("Enemy").Length;
+
+        if (enemyCount == 0)
+        {
+            SpawnEnemyWave(waveCount);
+            enemyScript.speed += 450.0f;
+        }
+
+    }
+
+    // Generate random spawn position for powerups and enemy balls
+    Vector3 GenerateSpawnPosition ()
+    {
+        float xPos = Random.Range(-spawnRangeX, spawnRangeX);
+        float zPos = Random.Range(spawnZMin, spawnZMax);
+        return new Vector3(xPos, 0, zPos);
+    }
+
+
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        Vector3 powerupSpawnOffset = new Vector3(0, 0, -15); // make powerups spawn at player end
+
+        // If no powerups remain, spawn a powerup
+        if (GameObject.FindGameObjectsWithTag("Powerup").Length == 0) // check that there are zero powerups
+        {
+            Instantiate(powerupPrefab, GenerateSpawnPosition() + powerupSpawnOffset, powerupPrefab.transform.rotation);
+        }
+
+        // Spawn number of enemy balls based on wave number
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }      
+        waveCount++;
+        ResetPlayerPosition(); // put player back at start
+    }
+
+    // Move player back to position in front of own goal
+    void ResetPlayerPosition ()
+    {
+        player.transform.position = new Vector3(0, 1, -7);
+        player.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        player.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+    }
+}
